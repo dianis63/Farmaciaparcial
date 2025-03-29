73109a4c83d8fd9610fde54866b3f519cc51c2cd
Javier Martinez <javiermartinezv33@gmail.com>
2025-03-28 21:28:23 -0600

    Adición de Get y Post de Ventas y Detalle Venta

--- APIJava/src/main/java/com/farmacia/sanrafael/APIJava/repository/DetalleVentaRepository.java
+++ APIJava/src/main/java/com/farmacia/sanrafael/APIJava/repository/DetalleVentaRepository.java
@@ -1,9 +1,8 @@
 package com.farmacia.sanrafael.APIJava.repository;
 import com.farmacia.sanrafael.APIJava.entities.DetalleVentaEntity;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DetalleVentaRepository {
+public interface DetalleVentaRepository extends JpaRepository<DetalleVentaEntity, Long> {
 }
