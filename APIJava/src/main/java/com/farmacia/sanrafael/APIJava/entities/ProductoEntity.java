3cda56627d5a9ec8f9771ed3ef2fb1f0738d8793
DianaOrtiz <diacarolavil@gmail.com>
2025-06-03 16:42:13 -0600

    Manejo de @OneToMany en ProductoEntity

--- APIJava/src/main/java/com/farmacia/sanrafael/APIJava/entities/ProductoEntity.java
+++ APIJava/src/main/java/com/farmacia/sanrafael/APIJava/entities/ProductoEntity.java
@@ -1,10 +1,12 @@
 package com.farmacia.sanrafael.APIJava.entities;
+import com.farmacia.sanrafael.APIJava.entities.DetalleVentaEntity;
+import com.farmacia.sanrafael.APIJava.entities.IngresoEntity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
-
+import java.util.List;
 import java.util.Date;
 
 @Setter
@@ -39,5 +41,12 @@ public class ProductoEntity {
     @Temporal(TemporalType.DATE)
     @Column(columnDefinition = "DATE")
     private Date fecha_vencimiento;
+
+    @OneToMany(mappedBy = "producto", cascade = CascadeType.PERSIST)
+    private List<DetalleVentaEntity> detallesVenta;
+
+    @OneToMany(mappedBy = "producto", cascade = CascadeType.PERSIST)
+    private List<IngresoEntity> ingresos;
+
 
 }
